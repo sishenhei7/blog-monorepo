b0e68befa93e18f804215fcdfaca09f508abf7ef
sishenhei7 <982505136@qq.com>
2022-01-25 18:30:55 +0800

    refactor: modify axios

--- packages/blog-vue3/src/plugins/axios.ts
+++ packages/blog-vue3/src/plugins/axios.ts
@@ -4,3 +4,21 @@
  * 2.在server端透传headers等参数
  * 3.记录log
  */
+import Axios from 'axios'
+import { PluginOptions } from '@/plugins'
+
+export default async ({ ctx, store, isClient, router, app }: PluginOptions) => {
+  const axiosTimeout = isClient ? 15000 : 6000
+  const baseURL = isClient ? process.env.API_URL_BROWSER : process.env.API_URL
+  const axios = Axios.create({
+    baseURL,
+    timeout: axiosTimeout,
+    withCredentials: false,
+    headers: {
+      Version: '1.0',
+      Accept: 'application/json, text/plain, */*',
+      'X-Requested-With': 'XMLHttpRequest'
+    }
+  })
+  app.config.globalProperties.$axios = axios
+}
