fca6a903ec82e13a43089d0b62d24579d79c6520
sishenhei7 <982505136@qq.com>
2022-02-07 11:50:20 +0800

    fix: fix ts error

--- packages/blog-vue3/types/global.d.ts
+++ packages/blog-vue3/types/global.d.ts
@@ -1,7 +1,17 @@
-export {}
+import { Context } from 'koa'
+import { Pinia } from 'pinia'
+import { Router } from 'vue-router'
 
 declare global {
   interface Window {
     __INITIAL_STATE__: any
   }
 }
+
+declare module '@vue/runtime-core' {
+  interface ComponentCustomOptions {
+    asyncData?(ctx: Context, store: Pinia, router: Router): Promise<void>
+  }
+}
+
+export {}
